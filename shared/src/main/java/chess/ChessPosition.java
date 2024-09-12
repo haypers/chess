d4692b31cb04bf7420047523e5144d6dcdcf516f
Hayden Perkes <haypers@gmail.com>
2024-09-12 10:25:06 -0600

    Added notation to override equals function durring class.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -39,10 +39,11 @@ public class ChessPosition {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) return true; //check if the memory address is the same.
+        if (o == null || getClass() != o.getClass()) return false; //check if the object is null or an incorrect class.
 
-        ChessPosition that = (ChessPosition) o;
+
+        ChessPosition that = (ChessPosition) o; //this is a typecast command! Because we allow comparisons to any object, ,not just other chess positions, we need to ensure that we can actually access the object as a chess peice object, so we are first checking if it is a chess position object, then we typecast it back into one, so we can pull values out and compare them.
         return row == that.row && col == that.col;
     }
 
