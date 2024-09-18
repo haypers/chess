6cde60072687957d47ecbb2c5e4c9cf3de74b58a
Hayden Perkes <haypers@gmail.com>
2024-09-18 10:43:12 -0600

    All tests passed. Had to overwrite equals function for board and pieces.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -1454,17 +1455,21 @@ public class ChessPiece {
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
 
-
-
-
-
-
-
-
-
-
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(pieceColor);
+        result = 31 * result + Objects.hashCode(type);
+        return result;
+    }
 
     @Override
     public String toString() {
