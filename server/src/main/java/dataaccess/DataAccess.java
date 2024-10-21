cf5ce66ecd682d43a8086c3bbfb4493432c2323b
Hayden Perkes <haypers@gmail.com>
2024-10-21 17:01:53 -0600

    made sure to throw response exception in interface

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import exception.ResponseException;
 import model.GameData;
 import model.PublicGameData;
 import model.UserData;
@@ -7,27 +8,27 @@ import java.util.ArrayList;
 
 public interface DataAccess {
 
-    boolean checkIfUsersExists(String userName);
+    boolean checkIfUsersExists(String userName) throws ResponseException;
 
-    boolean checkIfGameExists(int gameID);
+    boolean checkIfGameExists(int gameID) throws ResponseException;
 
-    boolean checkIfHashExists(String hash);
+    boolean checkIfHashExists(String hash) throws ResponseException;
 
-    void addUser(UserData data);
+    void addUser(UserData data) throws ResponseException;
 
-    boolean saveAuthToken(String userName, String authToken);
+    boolean saveAuthToken(String userName, String authToken) throws ResponseException;
 
-    boolean saveGameData(int gameID, GameData gameData);
+    boolean saveGameData(int gameID, GameData gameData) throws ResponseException;
 
-    ArrayList<PublicGameData> getAllGames();
+    ArrayList<PublicGameData> getAllGames() throws ResponseException;
 
-    GameData getGame(int gameID);
+    GameData getGame(int gameID) throws ResponseException;
 
-    boolean clearDatabase();
+    boolean clearDatabase() throws ResponseException;
 
-    String getPassHash(String userName);
+    String getPassHash(String userName) throws ResponseException;
 
-    String getUserFromToken(String authToken);
+    String getUserFromToken(String authToken) throws ResponseException;
 
-    boolean logoutUser(String token);
+    boolean logoutUser(String token) throws ResponseException;
 }
