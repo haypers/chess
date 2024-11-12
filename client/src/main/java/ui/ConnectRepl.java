3612edbc48934be5fbd1a00614ba7c5347d976c8
Hayden Perkes <haypers@gmail.com>
2024-11-12 09:53:01 -0700

    added notes from class

--- client/src/main/java/ui/ConnectRepl.java
+++ client/src/main/java/ui/ConnectRepl.java
@@ -1,27 +1,39 @@
 package ui;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 import static java.awt.Color.BLUE;
 
 public class ConnectRepl {
 
-
+//5: help, register(http), login(http), quit
+//6:help create game(http), list games(http), join game (http then websocket), join observer (http then websocket)
+        //help, redraw, leave (remove yourself as a player or observer, someone takes your place), make move(web socket), resign (end game, lose), highlight.
     public void run() {
         Scanner scanner = new Scanner(System.in);
         var result = "";
         while (!result.equals("quit")) {
-            System.out.print("♕>  ");
+            System.out.print("♕ > ");
             String line = scanner.nextLine();
 
             try {
-                //result = client.eval(line);
-                result = line;
-                System.out.println(result);
+                result = this.eval(line);
+                //result = line;
+                System.out.println("\u001b[34m" + result);
             } catch (Throwable e) {
                 var msg = e.toString();
                 System.out.print(msg);
             }
+        }
+    }
+
+    public String eval(String input){
+        if (Objects.equals(input, "test")){
+            return "nice!";
+        }
+        else{
+            return input;
         }
     }
 
