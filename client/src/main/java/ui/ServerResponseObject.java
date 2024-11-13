4b0e09d2a3eb0a16ab1ed604f3b76acfed428a45
Hayden Perkes <haypers@gmail.com>
2024-11-13 13:50:34 -0700

    Finished some bug fixes

--- client/src/main/java/ui/ServerResponseObject.java
+++ client/src/main/java/ui/ServerResponseObject.java
@@ -5,9 +5,14 @@ import chess.ChessGame;
 public class ServerResponseObject {
     public String username;
     public String authToken;
+    public String message;
 
     public String username(){
         return username;
+    }
+
+    public String message(){
+        return message;
     }
 
     public String authToken(){
